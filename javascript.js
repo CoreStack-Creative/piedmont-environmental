523072996450b94d09d7dfcaf1b05797bf0cc7e1
Baxter Caress <baxter.caresscsc@gmail.com>
2025-05-25 22:04:27 -0400

    qucky

--- javascript.js
+++ javascript.js
@@ -290,3 +290,25 @@ document.addEventListener("DOMContentLoaded", () => {
     carousel.scrollLeft = scrollPos;
   }, { passive: false });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const appearOnScroll = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+  );
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+});
